9a082add7365eb6921c1de56ee0e5bb6ade1bb2a
Rishabh <rishabhkhare314@gmail.com>
2025-12-27 01:26:56 +0530

    setup basic redux-tookit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  reset,
+} from "./store/slices/CounterSlice";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.count);
+  return (
+    <div className="root">
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Increment By Any Amount</button>
+      <input
+        type="number"
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+        onBlur={() => dispatch(incrementByAmount(Number(count)))}
+      />
+    </div>
+  );
+}
+
+export default App;
